3efcca66257761fb831452326e171321042457b4
Leo <lp29379@seeu.edu.mk>
2022-11-03 12:37:53 +0100

    .Net Commit

---
+++ WebApplication2/Controllers/AppointmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //    string today = DateTime.Now.ToShortDateString();
+        //    return Ok(today);
+        //}
+        public IActionResult Index(int id)
+        {
+            return Ok("No appointment for user" + id);
+        }
+        public IActionResult Getname([FromQuery(Name = "name")] string name)
+        {
+            return Ok("Hi" + name);
+        }
+        public IActionResult GetZokiName([FromQuery(Name = "name")] string name)
+        {
+            return Ok("Hi " + name + "You are Stupid");
+        }
+    }
+}
